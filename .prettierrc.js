fd409db20807e18be408a00d352adb47356eea03
Rafael Sales <rafael.moaz@yahoo.com.br>
2024-03-19 11:51:06 -0300

    second commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+const config = {
+  trailingComma: 'all', //virgula sobrando
+  tabWidth: 2, // tamanho do TAB
+  printWidth: 80, //largula da linha
+  semi: true, //para ponto e virgula
+  singleQuote: true, // aspas simples
+};
+
+module.exports = config;
