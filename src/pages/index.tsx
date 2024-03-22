97394dfaf5aecf5e7b469de1faa7d384d41bbb09
Rafael Sales <rafael.moaz@yahoo.com.br>
2024-03-21 22:47:02 -0300

    criado primeira request da api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,23 @@
+import { useEffect, useState } from 'react';
+import { PostData } from '../domain/posts/post';
+
+const getPosts = async (): Promise<PostData[]> => {
+  const posts = await fetch('https://shrouded-plains-22485-333d22db38bc.herokuapp.com/api/posts?populate=*');
+  const jsonPosts = await posts.json();
+  return jsonPosts.data;
+};
+
 export default function Home() {
-  return <h1>oi</h1>;
+  const [posts, setPosts] = useState<PostData[]>([]);
+  useEffect(() => {
+    getPosts().then((response) => setPosts(response));
+  }, [posts]);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <h2 key={post.id}>{post.attributes.title}</h2>
+      ))}
+    </div>
+  );
 }
