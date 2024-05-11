ca992fd5ebc7359e59032c127d597850f9d951ce
Rafael Sales <rafael.moaz@yahoo.com.br>
2024-05-11 15:59:09 -0300

    finalizado rota count

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,40 @@
-const Footer = () => {
-  return <p>Feito por Rafael Sales</p>;
+import { countAllPosts } from '@/src/data/posts/count-all-posts';
+import { getAllPosts } from '@/src/data/posts/get-all-posts';
+import { getPost } from '@/src/data/posts/get.posts';
+import { PostData } from '@/src/domain/posts/post';
+import { GetStaticPaths, GetStaticProps } from 'next';
+
+export type DynamicPostProps = {
+  post: PostData;
 };
 
-export default Footer;
+const DynamicPost = ({ post }: DynamicPostProps) => {
+  return <p>{post.attributes.title}</p>;
+};
+
+export default DynamicPost;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const numberOfPosts = await countAllPosts();
+  const posts = await getAllPosts(
+    `/?populate=*&pagination[start]=0&pagination[pagesize]=${numberOfPosts}`,
+  );
+  return {
+    paths: posts.map((post) => {
+      return {
+        params: {
+          slug: post.attributes.slug,
+        },
+      };
+    }),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const posts = await getPost(ctx.params?.slug);
+
+  return {
+    props: { post: posts[0] },
+  };
+};
