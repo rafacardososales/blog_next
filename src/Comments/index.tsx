512d20336245778c092b4b2e6514b12db5abeb02
Rafael Sales <rafael.moaz@yahoo.com.br>
2024-06-11 20:18:41 -0300

    consfigurando para suibir site estatico para netlify

--- src/Comments/index.tsx
+++ src/Comments/index.tsx
@@ -13,7 +13,7 @@ export const Comments = ({ slug, title }: CommentsProps) => {
       <DiscussionEmbed
         shortname="blogdorafa-1"
         config={{
-          url: `/post/${slug}`,
+          url: `https://vcap.me/:3000/post/${slug}`,
           identifier: slug,
           title: title,
           language: 'pt-BR',
