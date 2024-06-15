6dd28c69aa13348e8e2082df67ede0986242a332
Rafael Sales <rafael.moaz@yahoo.com.br>
2024-06-15 20:28:43 -0300

    atuazilado o formato dos card do menu principal

--- src/components/PostCover/styled.ts
+++ src/components/PostCover/styled.ts
@@ -1,6 +1,9 @@
 import styled, { css } from 'styled-components';
 
 export const Container = styled.img`
+  border-radius: 20px;
+  overflow: hidden;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.5);
   ${({ theme }) => css`
     max-width: 100%;
     margin-bottom: ${theme.spacings.medium};
