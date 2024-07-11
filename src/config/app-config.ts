20a79e7d12e2c70a4827f4166b2f2968c8ec496e
rafacardososales <rafael.moaz@yahoo.com.br>
2024-07-10 22:02:00 -0300

    atualizou a <Head> das categorias

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,5 +2,5 @@ export const API_URL = 'https://blogstrapi-production.up.railway.app';
 
 export const POST_URL = `${API_URL}/api/posts?populate=*`;
 
-export const SITE_NAME = 'Rafael Sales';
+export const SITE_NAME = 'Cheffy';
 export const SITE_URL = '';
